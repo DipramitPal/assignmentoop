aeaf14daccf0c63ccf267c5a88be9925491fc539
Dipramit <dipramit@iitkgp.ac.in>
2016-09-26 23:53:06 +0530

    Added the front End work of Login Panel

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+	    	    <div class="anyForm">
+					<div class="container white z-depth-5">
+						<div class="row">
+							<h2 class="center">Login</h2>
+							<form class="col s10 offset-s1" method="POST" action="core.php">
+								<div class="row">
+									<div class="input-field col s8 offset-s2">
+										<i class="material-icons prefix">account_circle</i>
+										<input id="userName" type="text" class="validate" name="uname">
+										<label for="userName">User Name</label>
+									</div>
+									<div class="input-field col s8 offset-s2">
+										<i class="material-icons prefix">vpn_key</i>
+			          					<input id="password" type="password" class="validate" name="pass">
+			          					<label for="password">Password</label>
+									</div>
+								</div>
+								<div class="row">
+										<div class="center">
+											<button class="btn waves-effect waves-light btn-logIn">Submit
+															<i class="material-icons right">send</i>
+  											</button>
+										</div>
+								</div>
+								
+							</form>
+						</div>
+					</div>
+				</div>
+
+    	
+
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
