b9e13c8ab2d6743c941997f4ad5598156eafa140
paras7735 <parasb10chd@iitkgp.ac.in>
2016-09-27 14:09:53 +0530

    concept

---
+++ sub.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Subhead Page</title>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	 <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+          
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <style type="text/css">.card-panel{height: 400px !important;width: 400px !important;}
+    .carousel {margin-top: -50px;}
+    .headd{text-align: center;}</style>
+</head>
+<body>
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#one">My Tasks</a></li>
+        <li><a href="#two">All Tasks</a></li>
+        <li><a href="collapsible.html">My Info</a></li>
+      </ul>
+    </div>
+  </nav>
+        <div class="container" id="one">
+        <h1 class="headd">My Tasks</h1>
+  <div class="carousel">
+ <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+  <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+   <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12 ">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div></div>
+    <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+     <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+  </div>
+  <div class="row"><br><br><br></div>
+  </div><div class="container">
+  <hr></div>
+  <div class="container" id="two">
+    <h1 class="headd">All Tasks</h1>
+  <div class="carousel">
+ <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+  <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+   <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12 ">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div></div>
+    <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+     <div class="carousel-item">
+    <div class="row">
+      <div class="col s12 m12">
+        <div class="card-panel teal">
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+          </span>
+        </div>
+      </div>
+    </div> </div>
+  </div>
+  </div>
+  <div class="row"><br><br><br></div>
+  </div><div class="container">
+  <hr></div>
+      <script type="text/javascript"> $(document).ready(function(){
+      $('.carousel').carousel();
+    });</script>
+</body>
+</html>
